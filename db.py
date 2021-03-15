7584f9498ea27910ce4997ae53b89cbfba7abdf7
Arjun <cosmicpepe2001@gmail.com>
2021-03-15 18:38:03 +0530

    production db

--- db.py
+++ db.py
@@ -1,10 +1,9 @@
 from flask import Flask
 import pymongo
-
+import os
 app=Flask(__name__)
 
-# CONNECTION_STRING = "mongodb+srv://swarnabha:swarnabhaflask@cluster0.sbgtv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-CONNECTION_STRING = "mongodb://localhost:27017/userDb"
+CONNECTION_STRING = os.getenv("CONNECTION_STRING")
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('userDb')
 user_collection = pymongo.collection.Collection(db, 'usersColl')
